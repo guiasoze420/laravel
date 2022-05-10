0743925e362b5306d92bbb3862c04a53b48eee8e
guiasoze420 <guiasoze420@gmail.com>
2022-05-11 07:42:24 +0900

    new

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 //Route::get('/', function () {
 //    return view('welcome');
 //});
@@ -21,6 +10,33 @@
 
 Auth::routes();
 
-Route::get('/', 'ItemController@index')->name('index');
+Route::get('/item', 'ItemController@index')->name('index');
 
 Route::get('/detail/{id}', 'ItemController@detail')->name('detail');
+
+//User認証不要
+Route::get('/', function() {
+	return redirect('/home');
+});
+
+//Userログイン後
+Route::group(['middleware' => 'auth:user'], function() {
+	Route::get('/home', 'HomeController@index')->name('home');
+});
+
+//Admin認証不要
+Route::group(['prefix' => 'admin'], function() {
+	Route::get('/', function() {
+		return redirect('/admin/home');
+	});
+	Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+	Route::post('login', 'Admin\LoginController@login');
+});
+
+//Adminログイン後
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
+	Route::post('logout', 'Admin\LoginController@logout')->name('admin.logout');
+	Route::get('home', 'Admin\HomeController@index')->name('admin.home');
+	Route::get('item', 'Admin\ItemController@index')->name('admin.index');
+	Route::get('detail/{id}', 'Admin\ItemController@detail')->name('admin.detail');
+});
